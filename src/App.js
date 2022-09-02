a429895e2d6893a5bd85561561426403acb033e7
nagavarshith <nagqvarshith123@gmail.com>
2022-09-02 09:34:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
 import './App.css';
-
+import resume from './data/db.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Card, CardHeader, CardBody, CardText, ListGroup, ListGroupItem } from 'reactstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className='mt-2'>
+        <h2>{resume.name}</h2>
+        <span>{resume.email}</span>
+        <span>{resume.mobile}</span>
+        <span>{resume.address}</span>
+        <Container>
+          <Card className='col-2 my-2'>
+            <img
+              alt="Sample"
+              src="./favicon.jpeg"
+            />
+          </Card>
+        </Container>
+      </Container>
+      <Container className='mt-3'>
+        <Card>
+          <CardHeader tag="h6">
+            Education
+          </CardHeader>
+          <ListGroup flush>
+            {/* {resume.education.map((ed)=>
+             (
+              <li key={ed.id}>{ed.course}</li>
+
+            ))} */}
+            {
+              resume.education.map((ed) => (
+                <ListGroupItem key={ed.id}>{ed.course} in {ed.institute} with {ed.score}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </Card>
+      </Container>
+      <Container className='mt-3'>
+        <Card>
+          <CardHeader tag="h6">
+            Internships
+          </CardHeader>
+          <ListGroup flush>
+            {/* {resume.education.map((ed)=>
+             (
+              <li key={ed.id}>{ed.course}</li>
+
+            ))} */}
+            {
+              resume.internships.map((ed) => (
+                <ListGroupItem >{ed}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </Card>
+      </Container>
+      <Container className='mt-3'>
+        <Card>
+          <CardHeader tag="h6">
+            Certifications
+          </CardHeader>
+          <ListGroup flush>
+            {/* {resume.education.map((ed)=>
+             (
+              <li key={ed.id}>{ed.course}</li>
+
+            ))} */}
+            {
+              resume.certificates.map((ed) => (
+                <ListGroupItem>{ed}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </Card>
+      </Container>
+      <Container className='mt-3'>
+        <Card>
+          <CardHeader tag="h6">
+            Projects
+          </CardHeader>
+          <ListGroup flush>
+            {/* {resume.education.map((ed)=>
+             (
+              <li key={ed.id}>{ed.course}</li>
+
+            ))} */}
+            {
+              resume.projects.map((ed) => (
+                <ListGroupItem>{ed}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </Card>
+      </Container>
+      <Container className='mt-3'>
+        <Card>
+          <CardHeader tag="h6">
+            Skills
+          </CardHeader>
+          <ListGroup flush>
+            {/* {resume.education.map((ed)=>
+             (
+              <li key={ed.id}>{ed.course}</li>
+
+            ))} */}
+            {
+              resume.skills.map((ed) => (
+                <ListGroupItem>{ed}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </Card>
+      </Container>
+      <Container className='mt-3'>
+        <Card>
+          <CardHeader tag="h6">
+            Hobbies
+          </CardHeader>
+          <ListGroup flush>
+            {/* {resume.education.map((ed)=>
+             (
+              <li key={ed.id}>{ed.course}</li>
+
+            ))} */}
+            {
+              resume.hobbies.map((ed) => (
+                <ListGroupItem>{ed}</ListGroupItem>
+              ))
+            }
+          </ListGroup>
+        </Card>
+      </Container>
+
+
     </div>
   );
 }
